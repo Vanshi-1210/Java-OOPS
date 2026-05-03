503ae09bf5cf4f9cd2e4ca8865480802ce679cdc
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-05-03 18:23:06 +0530

    Added method overloading demonstration program

---
+++ MethodOverloadingDemo.java
@@ -0,0 +1,25 @@
+// Program demonstrating method overloading in Java
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(String name){
+        System.out.println(name);
+    }
+    public void printInfo(int age){
+        System.out.println(age);
+    }
+    public void printInfo(String name,int age){
+        System.out.println(name+","+age);
+    }
+}
+
+public class MethodOverloadingDemo {
+ public static void main(String[] args) {
+    Student s1=new Student();
+    s1.name="vanshika";
+    s1.age=20;
+    s1.printInfo(s1.name,s1.age);
+ }    
+
+}
